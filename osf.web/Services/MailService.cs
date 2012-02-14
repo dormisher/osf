d940b6e4f5ccfab1734da562b817bfbee8b12f80
Dan Ormisher <dormisher@gmail.com>
2012-02-14 13:55:25 +0000

    uncommented mail sending code

--- osf.web/Services/MailService.cs
+++ osf.web/Services/MailService.cs
@@ -15,52 +15,8 @@ namespace osf.web.Services
 
 			using (var smtpClient = new SmtpClient())
 			{
-				//smtpClient.Send(msg);
+				smtpClient.Send(msg);
 			}
 		}
-
-		//internal void ValidateGrantApplication(GrantApplicationModel grantApplication, ModelStateDictionary modelState, HttpPostedFileBase file)
-		//{
-		//    ValidateContactModel(grantApplication.Contact1, modelState);
-		//    ValidateContactModel(grantApplication.Contact2, modelState);
-
-		//    if (string.IsNullOrEmpty(grantApplication.InsuranceNumber))
-		//    {
-		//        modelState.AddModelError("InsuranceNumber", "please enter your insurance number");
-		//    }
-
-		//    if (file.ContentLength == 0)
-		//    {
-		//        modelState.AddModelError("child", "please upload child protection policy");
-		//    }
-		//}
-
-		//private void ValidateContactModel(ContactModel contactModel, ModelStateDictionary modelState)
-		//{
-		//    if (string.IsNullOrEmpty(contactModel.Name))
-		//    {
-		//        modelState.AddModelError("Name", "please enter your name");
-		//    }
-
-		//    if (string.IsNullOrEmpty(contactModel.Email))
-		//    {
-		//        modelState.AddModelError("Email", "please enter your email address");
-		//    }
-
-		//    if (string.IsNullOrEmpty(contactModel.Address))
-		//    {
-		//        modelState.AddModelError("Address", "please enter your address");
-		//    }
-
-		//    if (string.IsNullOrEmpty(contactModel.Position))
-		//    {
-		//        modelState.AddModelError("Email", "please enter your position");
-		//    }
-
-		//    if (string.IsNullOrEmpty(contactModel.Telephone))
-		//    {
-		//        modelState.AddModelError("Email", "please enter your phone number");
-		//    }
-		//}
 	}
 }
