c42f23c1613fa48a70bd52825bc083b035e37264
Dan Ormisher <dormisher@gmail.com>
2012-02-18 19:01:13 +0000

    done about page, started on pdf's

---
+++ osf.web/Controllers/BrochuresController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.IO;
+using System;
+
+namespace osf.web.Controllers
+{
+    public class BrochuresController : Controller
+    {
+        public ActionResult Pdf()
+        {
+            string pdfPath = HttpContext.Server.MapPath("~/content/pdf1.pdf");
+
+            FileStream fileStream = System.IO.File.OpenRead(pdfPath);
+
+            HttpContext.Response.AddHeader("content-disposition", "attachment; filename=form.pdf");
+
+            return new FileStreamResult(fileStream, "application/pdf");
+        }
+    }
+}
