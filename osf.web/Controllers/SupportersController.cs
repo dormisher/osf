381368706a2f239079813d3f989d212459bca8b0
Dan Ormisher <dormisher@gmail.com>
2012-02-19 20:33:02 +0000

    finished everything now apart from paypal & testing in browsers

---
+++ osf.web/Controllers/SupportersController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace osf.web.Controllers
+{
+    public class SupportersController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
